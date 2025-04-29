614e6534eb3f84e658c508f17d6b639c4d16ec16
RKVanier <6241246@edu.vaniercollege.qc.ca>
2025-04-29 03:03:27 -0400

    Updated Flight.java with new feature

--- src/main/java/org/example/Flight.java
+++ src/main/java/org/example/Flight.java
@@ -1,5 +1,13 @@
 package org.example;
 
-public class Flight {
+import java.time.LocalDateTime;
+import java.util.List;
 
+public abstract class Flight {
+    protected int flightNumber;
+    protected LocalDateTime departureDateTime;
+    protected LocalDateTime arrivalDateTime;
+    protected String origin;
+    protected String destination;
+    protected double price;
 }
